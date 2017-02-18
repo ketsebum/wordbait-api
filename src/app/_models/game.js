37740ff13dc683a1edf925adba7054cbc4970efa
Ketse <snunleys@gmail.com>
2017-02-18 13:55:55 -0500

    Removing JS and Map files

--- src/app/_models/game.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Game = (function () {
-    function Game() {
-    }
-    return Game;
-}());
-exports.Game = Game;
-//# sourceMappingURL=game.js.map
--- src/app/_models/move.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Move = (function () {
-    function Move() {
-    }
-    return Move;
-}());
-exports.Move = Move;
-//# sourceMappingURL=move.js.map
